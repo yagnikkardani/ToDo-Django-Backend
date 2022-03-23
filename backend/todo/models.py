1763a2743fe15a8522fdac69b1bdbe63478fceeb
YK <kardaniyagnik@yagnik.com.au>
2022-03-23 12:31:04 +1000

    ToDo Backend

---
+++ backend/todo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    todoitem = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.todoitem
